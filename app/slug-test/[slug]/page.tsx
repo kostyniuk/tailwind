0149e0f1d38544f25b42749e05e1bf63236d0f9b
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-09-24 23:03:10 +0200

    Refactor SlugTest component to handle async params for dynamic slug rendering

--- app/slug-test/[slug]/page.tsx
+++ app/slug-test/[slug]/page.tsx
@@ -1,3 +1,4 @@
-export default function SlugTest({ params }: { params: { slug: string } }) {
-    return <div>Slug dynamic page test: {params.slug}</div>;
+export default async function SlugTest({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
+    return <div>Slug dynamic page test: {slug}</div>;
 }
